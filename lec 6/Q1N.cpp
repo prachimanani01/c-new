45de7c39358bc89b93abac2285d3ac7c7a56c8d2
prachi <144036679+prachimanani01@users.noreply.github.com>
2024-01-25 17:34:53 +0530

    Add files via upload

---
+++ lec 6/Q1N.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+    int a;
+		public  :
+	        void setData(char d)
+			{
+	            cout <<"Enter the "<< d <<" :- ";
+	            cin  >>a;
+	        }
+	        void getData()
+			{
+	            cout << "A : "<< a << endl;
+	        }
+	        bool operator < (A&c)
+			{
+	            if (this -> a < c.a)
+				{
+	                return true;
+	            }
+	            else
+				{
+	                return false;
+	            }
+	        }
+};
+int main()
+{
+
+    A a,b;
+    a.setData('A');
+    b.setData('B');
+
+    if(a < b)
+	{
+        cout <<"b is larger......."<<endl;
+    }
+    else
+	{
+        cout <<"a is larger........ "<<endl;
+    }
+}
